de8eef17cfd49d0291e8e0c6aacc9e4507b7878b
Mark Friedenbach <mark@monetize.io>
2013-08-06 13:42:36 -0700

    Switch initial distribution to Freicoin's linearly-decreasing reward.

--- src/main.cpp
+++ src/main.cpp
@@ -1269,16 +1269,12 @@ bool VerifyBudget(const std::map<CTxDestination, mpq>& mapBudget,
 
 mpq GetInitialDistributionAmount(int nHeight)
 {
-    mpq nSubsidy = 50 * COIN;
-    int halvings = nHeight / Params().SubsidyHalvingInterval();
-
-    // Force block reward to zero when right shift is undefined.
-    if (halvings >= 64)
-        return nFees;
-
-    // Subsidy is cut in half every 210,000 blocks which will occur approximately every 4 years.
-    nSubsidy >>= halvings;
-
+    const int nEquilibriumHeight = Params().EquilibriumHeight();
+    mpq nSubsidy = 0;
+    if ( nHeight < nEquilibriumHeight )
+        nSubsidy = (nEquilibriumHeight-nHeight) *
+                   Params().InitialSubsidy()    /
+                   nEquilibriumHeight;
     return nSubsidy;
 }
 
